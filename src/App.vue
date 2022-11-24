c62e4a8b2bd5fd9e81d1cd729934f9e38be0d1c1
jakubbedra <jakubbedra@wp.pl>
2022-11-24 01:38:17 +0100

    asdf

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <Header></Header>
+  <div>
+    <Header></Header>
+    <Logout></Logout>
+  </div>
   <router-view/>
 </template>
 
 <script setup>
 import {RouterView, RouterLink} from 'vue-router'
 import Header from "./components/Header.vue";
+import Logout from "./components/Logout.vue";
 </script>
---
+++ src/components/Logout.vue
@@ -0,0 +1,10 @@
+﻿<template>
+  <h2><button>Logout</button></h2>
+</template>
+
+<style>
+button {
+  float: right;
+  display: inline-block;
+}
+</style>
